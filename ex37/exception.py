5150f6cbad612679cd219149071d8c256d68bc5e
doreality <dieofreality@gmail.com>
2023-05-04 21:02:50 +0800

    update

---
+++ ex37/exception.py
@@ -0,0 +1,32 @@
+"""
+Test `try`, `except`, `finally`, `raise` keyword
+"""
+# 2 ways to use try-except
+#   1. 指定具体要捕获的异常，例如 ValueError，TypeError
+#   2. 如果不指定，就要 log fully stack trace，
+#      否则很容易出现捕获了异常但是不知道到底发生了什么
+#      在 except Exception as ex:
+#        logging.exception('Caught an error.')
+
+# Common exceptions:
+# ImportError
+# IndexError
+# NameError
+# SyntaxError
+# TypeError
+# ValueError
+
+try:
+    print(1)
+    print(2 / 0)  # Zero Division
+    # print('a' + 1) # Type error
+except ZeroDivisionError:
+    print("Division is Zero!")
+except ValueError:
+    print('Value is not expected')
+except TypeError:
+    print('Type is wrong')
+else:  # only execute when try is done completely (no exception)
+    print('hello')
+finally:  # print anyway, after try or except
+    print('Print anyway.')
--- ex37/yield.py
+++ ex37/yield.py
@@ -11,7 +11,8 @@ def count_up_to(num):
         a_num += 1
 
 
-# the count_up_to() executes firstly,  then the loop.
+# 每次循环时，count_up_to() 执行到 yield，然后停止；
+# 等到下一轮循环开始，再从上次停止的位置继续，执行到下一个 yield
 for i in count_up_to(5):
     print(i)
 
